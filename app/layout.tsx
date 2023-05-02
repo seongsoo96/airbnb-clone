583066325fa67f59a2c1a35f2365c33ac4b37a7a
seongsoo96 <seongsoo.dev@gmail.com>
2023-05-02 16:31:24 +0900

    feat: Implement listing creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import ClientOnly from './components/ClientOnly';
-import RegisterModal from './components/modals/RegisterModal';
 import Navbar from './components/navbar/Navbar';
 
 import './globals.css';
 import { Nunito } from 'next/font/google';
 import ToasterProvider from './providers/ToasterProvider';
+import RegisterModal from './components/modals/RegisterModal';
 import LoginModal from './components/modals/LoginModal';
+import RentModal from './components/modals/RentModal';
 import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
@@ -28,11 +29,12 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <RentModal />
           <LoginModal />
           <RegisterModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
